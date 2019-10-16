dea4e06e45391f0d9cae17767eba8357daae7901
Jairun Diemert <jsd25@students.uwf.edu>
2019-10-16 15:55:16 -0500

    file input and formatted output wrking

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,45 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 int main() {
-	cout << "hi" << endl;
-	system("pause");
+  ifstream myFile;
+  myFile.open("fourXfour.in");
+  string line;
+  char tempChar;
+  string lineArr[4];
+  int row = 4;
+  int column = 4;
+  int k = 0;
+
+
+  char mines[row][column];
+  int counter = 0;
+  if (!myFile.is_open()){
+    cout << "File open ERROR" << endl;
+  }
+  while (getline(myFile, line)) {
+
+    for (int i = 0; i < column; ++i){
+      mines[k][i] = line[i];
+      cout << "[" << k << "]" << "[" << i << "] : " << mines[k][i] << endl;
+    }
+    
+    ++k;
+
+  }
+  cout << "\n";
+  cout << "   0  1  2  3" << endl;
+  cout << " +--+--+--+--+" << endl;
+  for (int i = 0; i < row; ++i){
+    cout << i;
+    for (int j = 0; j < column; ++j){
+      cout << "|" << mines[i][j] << " ";
+    }
+    cout << "|";
+    cout << endl;
+    cout << " +--+--+--+--+" << endl;
+  }
 }
+
