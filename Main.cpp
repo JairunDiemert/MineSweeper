b01acfbaffafbf0cf5e46c13c78afa67a4f33460
Jairun Diemert <jsd25@students.uwf.edu>
2019-10-16 17:05:37 -0500

    Functions derived for input file.

--- Main.cpp
+++ Main.cpp
@@ -3,43 +3,59 @@
 #include <string>
 using namespace std;
 
-int main() {
-  ifstream myFile;
-  myFile.open("fourXfour.in");
-  string line;
-  char tempChar;
-  string lineArr[4];
-  int row = 4;
-  int column = 4;
-  int k = 0;
+int const row = 4;
+int const column = 4;
 
-
-  char mines[row][column];
-  int counter = 0;
+void openFile(ifstream& myFile){
+  string fileName;
+  cout << "File Name: ";
+  cin >> fileName;
+  
+  myFile.open(fileName.c_str());
   if (!myFile.is_open()){
     cout << "File open ERROR" << endl;
   }
-  while (getline(myFile, line)) {
-
-    for (int i = 0; i < column; ++i){
-      mines[k][i] = line[i];
-      cout << "[" << k << "]" << "[" << i << "] : " << mines[k][i] << endl;
-    }
-    
-    ++k;
-
+  if (myFile.is_open()){
+    cout << "File is OPEN" << endl;
   }
-  cout << "\n";
-  cout << "   0  1  2  3" << endl;
-  cout << " +--+--+--+--+" << endl;
-  for (int i = 0; i < row; ++i){
-    cout << i;
-    for (int j = 0; j < column; ++j){
-      cout << "|" << mines[i][j] << " ";
+}
+void getMines(ifstream& myFile, char mines[row][column] ){
+  openFile(myFile);
+  int k = 0;
+  string line;
+    while (getline(myFile, line)) {
+      for (int i = 0; i < column; ++i){
+        mines[k][i] = line[i];
+      }
+      ++k;
     }
-    cout << "|";
-    cout << endl;
+  }
+  void displayBoard(char board[row][column]){
+    cout << "\n";
+    cout << "   0  1  2  3" << endl;
     cout << " +--+--+--+--+" << endl;
+    for (int i = 0; i < row; ++i){
+      cout << i;
+      for (int j = 0; j < column; ++j){
+        cout << "|" << board[i][j] << " ";
+      }
+      cout << "|";
+      cout << endl;
+      cout << " +--+--+--+--+" << endl;
+    }
   }
+
+
+
+int main() {
+
+  ifstream myFile;
+  char mines[row][column];
+
+  getMines(myFile, mines);
+  displayBoard(mines);
+  
+  
+  
 }
 
